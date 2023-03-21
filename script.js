352fd1fd86ffeba9e74f750812ccb387ab6b5aba
Khizar Ahmad <khizarahmad313@gmail.com>
2023-03-21 12:15:54 +0530

    bug fixes

--- script.js
+++ script.js
@@ -23,20 +23,32 @@ fetch("https://api.github.com/users") //data fetch request
   .then((data) => {
     let cards = document.getElementById("cardsContainer");
     cards.innerHTML = null;
-    //creating a for loop to to displaying data for i(length of data) times to create a card for the data
+    //running a for loop till the length of data for creating user info and appending it to main div(cardsContainer)
     for (let i = 0; i < data.length; i++) {
       let div = document.createElement("div");
       div.setAttribute("class", "card");
-
+      //creating a img element for each user
       let userImage = document.createElement("img");
       userImage.setAttribute("class", "profileImage");
       userImage.setAttribute("src", data[i].avatar_url);
       userImage.setAttribute("id", "avatar");
-
+      //creating a userName element for each user
       let userName = document.createElement("h3");
       userName.setAttribute("id", "userName");
       userName.innerText = data[i].login;
+      //creating a follow element for each user and creating a random number to display followres and following
+      let userFollow = document.createElement("p");
+      userFollow.setAttribute("id", "follow");
+      userFollow.innerText =
+        "🢚 " +
+        " Followers " +
+        (Math.floor(Math.random() * 10000) + 1) +
+        " ⦾ " +
+        " Following " +
+        (Math.floor(Math.random() * 100) + 1) +
+        " 🢘 ";
 
+      //creating a anchor element for each user
       let userLink = document.createElement("a");
       userLink.setAttribute("id", "profileLink");
       userLink.setAttribute("class", "profileButton");
@@ -44,7 +56,7 @@ fetch("https://api.github.com/users") //data fetch request
       userLink.setAttribute("target", "_blank");
       userLink.innerText = "Profile Link";
 
-      div.append(userImage, userName, userLink);
-      cards.append(div);
+      div.append(userImage, userName, userFollow, userLink); //appending the userinfo(userImage, userName and userLink) to the div(cards)
+      cards.append(div); //appending cards which containing userinfo to the cardsContainer
     }
   });
